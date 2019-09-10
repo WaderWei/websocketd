51aae97034baaac61fd4c11cda9160462dfc0064
wade <745696658@qq.com>
2019-09-10 20:35:23 +0800

    Initial commit

---
+++ src/main/java/wade/wei/WebsocketdApplication.java
@@ -0,0 +1,13 @@
+package wade.wei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketdApplication.class, args);
+    }
+
+}
